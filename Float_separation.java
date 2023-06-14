934292029dca376f74bb1848da88cc797285c611
amir-aa <50378368+amir-aa@users.noreply.github.com>
2023-06-14 15:07:34 +0330

    Create Float_separation.java
    this code separates the a number as a constant NUMBER to decimal and integer parts

---
+++ Float_separation.java
@@ -0,0 +1,9 @@
+public class sp1 {
+	public final static double NUMBER=139.76;
+	public static void main(String[] args) {
+		int a= (int)NUMBER;
+		double b= NUMBER-a;
+		System.out.printf("%6.2f Is the float part and Integer part is "+a,b);
+		
+
+	}
